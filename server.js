58818a5f35320008ce6a89295658c99c32d352fd
Debanjan Dasgupta <69705770+Debanjan-IT@users.noreply.github.com>
2022-07-09 17:31:59 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -53,30 +53,30 @@ const init = async () => {
         const numberRows = (await page.$$('#mp4 > table > tbody > tr')).length
         let data = []
         for (let index = 1; index <= numberRows; index++) {
-          await page.waitForSelector(`#mp4 > table > tbody > tr:nth-child(${index}) > td:nth-child(1) > a`)
-          let element1 = await page.$(`#mp4 > table > tbody > tr:nth-child(${index}) > td:nth-child(1) > a`)
-          await page.waitForSelector('#mp4 > table > tbody > tr')
-          await page.waitForSelector(`#mp4 > table > tbody > tr:nth-child(${index}) > td:nth-child(2)`)
-          let element2 = await page.$(`#mp4 > table > tbody > tr:nth-child(${index}) > td:nth-child(2)`)
-          await page.waitForSelector('#mp4 > table > tbody > tr')
-          const page1 = await browser.newPage();
-          await page1.goto(websiteUrl);
-          await page1.focus('#txt-url')
-          await page1.keyboard.type(url)
-          await page1.click("#btn-submit")
-          await page1.waitForSelector(`#mp4 > table > tbody > tr:nth-child(${index}) > td.txt-center > a`)
-          await page1.click(`#mp4 > table > tbody > tr:nth-child(${index}) > td.txt-center > a`)
-          await page1.waitForSelector("#process-result > div > a")
-          let link = await page1.$("#process-result > div > a")
-          let returnVal = {
-            format: await page.evaluate(el => el.textContent, element1),
-            size: await page.evaluate(el => el.textContent, element2),
-            link: await page1.evaluate(el => el.href, link)
-          }
-          data.push(returnVal)
+            await page.waitForSelector(`#mp4 > table > tbody > tr:nth-child(${index}) > td:nth-child(1) > a`)
+            let element1 = await page.$(`#mp4 > table > tbody > tr:nth-child(${index}) > td:nth-child(1) > a`)
+            await page.waitForSelector('#mp4 > table > tbody > tr')
+            await page.waitForSelector(`#mp4 > table > tbody > tr:nth-child(${index}) > td:nth-child(2)`)
+            let element2 = await page.$(`#mp4 > table > tbody > tr:nth-child(${index}) > td:nth-child(2)`)
+            await page.waitForSelector('#mp4 > table > tbody > tr')
+            const page1 = await browser.newPage();
+            await page1.goto(websiteUrl);
+            await page1.focus('#txt-url')
+            await page1.keyboard.type(url)
+            await page1.click("#btn-submit")
+            await page1.waitForSelector(`#mp4 > table > tbody > tr:nth-child(${index}) > td.txt-center > a`)
+            await page1.click(`#mp4 > table > tbody > tr:nth-child(${index}) > td.txt-center > a`)
+            await page1.waitForSelector("#process-result > div > a")
+            let link = await page1.$("#process-result > div > a")
+            let returnVal = {
+                format: await page.evaluate(el => el.textContent, element1),
+                size: await page.evaluate(el => el.textContent, element2),
+                link: await page1.evaluate(el => el.href, link)
+            }
+            data.push(returnVal)
         }
         await browser.close()
-        return data
+        return {title, thumbnail, data}
       } catch (error) {
         console.log(error);
       }
